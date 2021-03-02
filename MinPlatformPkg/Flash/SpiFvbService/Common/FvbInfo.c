18327f6ec1fe20acc5564b366378dff0ec56a515
John Schock <joschock@microsoft.com>
2021-03-02 13:35:58 -0800

    Update SpiFvbService FvbInfo to allow usage of dynamic PCDs for varstore location.

--- MinPlatformPkg/Flash/SpiFvbService/Common/FvbInfo.c
+++ MinPlatformPkg/Flash/SpiFvbService/Common/FvbInfo.c
@@ -12,8 +12,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 #define FIRMWARE_BLOCK_SIZE         0x10000
 #define FVB_MEDIA_BLOCK_SIZE        FIRMWARE_BLOCK_SIZE
 
-#define NV_STORAGE_BASE_ADDRESS     FixedPcdGet32(PcdFlashNvStorageVariableBase)
-#define SYSTEM_NV_BLOCK_NUM         ((FixedPcdGet32(PcdFlashNvStorageVariableSize)+ FixedPcdGet32(PcdFlashNvStorageFtwWorkingSize) + FixedPcdGet32(PcdFlashNvStorageFtwSpareSize))/ FVB_MEDIA_BLOCK_SIZE)
+#define NV_STORAGE_BASE_ADDRESS     PcdGet32(PcdFlashNvStorageVariableBase)
+#define SYSTEM_NV_BLOCK_NUM         ((PcdGet32(PcdFlashNvStorageVariableSize)+ PcdGet32(PcdFlashNvStorageFtwWorkingSize) + PcdGet32(PcdFlashNvStorageFtwSpareSize))/ FVB_MEDIA_BLOCK_SIZE)
 
 typedef struct {
   EFI_PHYSICAL_ADDRESS        BaseAddress;
@@ -30,11 +30,11 @@ EFI_FVB2_MEDIA_INFO mPlatformFvbMediaInfo[] = {
   // Systen NvStorage FVB
   //
   {
-    NV_STORAGE_BASE_ADDRESS,
+    0, //NV_STORAGE_BASE_ADDRESS,
     {
       {0,}, //ZeroVector[16]
       EFI_SYSTEM_NV_DATA_FV_GUID,
-      FVB_MEDIA_BLOCK_SIZE * SYSTEM_NV_BLOCK_NUM,
+      0, //FVB_MEDIA_BLOCK_SIZE * SYSTEM_NV_BLOCK_NUM,
       EFI_FVH_SIGNATURE,
       0x0004feff, // check MdePkg/Include/Pi/PiFirmwareVolume.h for details on EFI_FVB_ATTRIBUTES_2
       sizeof (EFI_FIRMWARE_VOLUME_HEADER) + sizeof (EFI_FV_BLOCK_MAP_ENTRY),
@@ -44,7 +44,7 @@ EFI_FVB2_MEDIA_INFO mPlatformFvbMediaInfo[] = {
       2,    //Revision
       {
         {
-          SYSTEM_NV_BLOCK_NUM,
+          0, //SYSTEM_NV_BLOCK_NUM,
           FVB_MEDIA_BLOCK_SIZE,
         }
       }
@@ -66,6 +66,11 @@ GetFvbInfo (
 {
   UINTN                       Index;
   EFI_FIRMWARE_VOLUME_HEADER  *FvHeader;
+
+  //Need to update to use dynamic PCD for FW volume locations.
+  mPlatformFvbMediaInfo[0].BaseAddress = NV_STORAGE_BASE_ADDRESS;
+  mPlatformFvbMediaInfo[0].FvbInfo.FvLength = FVB_MEDIA_BLOCK_SIZE * SYSTEM_NV_BLOCK_NUM;
+  mPlatformFvbMediaInfo[0].FvbInfo.BlockMap[0].NumBlocks =SYSTEM_NV_BLOCK_NUM;
 
   for (Index = 0; Index < sizeof (mPlatformFvbMediaInfo) / sizeof (EFI_FVB2_MEDIA_INFO); Index++) {
     if (mPlatformFvbMediaInfo[Index].BaseAddress == FvBaseAddress) {
